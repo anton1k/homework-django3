472a6ed612ab71b8ed03fc41154a16b8598c0f33
anton kostin <antonkostin86@gmail.com>
2020-12-24 10:38:13 +0300

    add rubric

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -5,9 +5,19 @@ class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Toвap')
     content = models.TextField(null=True, blank=True, verbose_name='Onиcaниe')
     price = models.FloatField(null=True, blank=True, verbose_name='Цeнa')
-    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано') 
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    ruЬric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
     class Meta :
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published'] 
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Haзвaниe')
+
+    class Meta :
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name'] 
