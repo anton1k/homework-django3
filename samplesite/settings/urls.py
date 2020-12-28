b962bc627a3082a5cabc945ee9f6ec55238dadf9
anton kostin <antonkostin86@gmail.com>
2020-12-28 10:52:46 +0300

    add static

--- samplesite/settings/urls.py
+++ samplesite/settings/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bboard/', include('samplesite.bboard.urls'))
 ]
+
+
+if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    print(settings.STATIC_ROOT)
+    print(1111111)
